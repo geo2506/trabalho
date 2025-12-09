f2c6495fdaf2fbcb1645e3a3d4f0987fde0e2554
geo2506 <geovana.santos25@escola.pr.gov.br>
2025-12-09 09:23:42 -0300

    java script

---
+++ java script.js
@@ -0,0 +1,10 @@
+<script>
+    // Seleciona o botão pelo seu seletor (neste caso, a tag 'button' é única)
+    var meuBotao = document.querySelector('button');
+
+    // Adiciona um "ouvinte de evento" (event listener) para o clique
+    meuBotao.addEventListener('click', function() {
+        // Exibe o alerta com o texto especificado
+        alert('Você clicou no botão!');
+    });
+</script>
